62eb0514a9abe2905fee6b8e3b30d9568a7f16e8
Duc Vu <de.yd315@gmail.com>
2019-04-12 08:54:31 -0500

    added reducer and store

---
+++ friends/src/reducers/friends.js
@@ -0,0 +1,13 @@
+const initialState = {
+    friends: []
+};
+
+const friends = (state=initialState, action) => {
+    switch (action) {
+        default:
+        return state
+    }
+}
+
+export default friends;
+
