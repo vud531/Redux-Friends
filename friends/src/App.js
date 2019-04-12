03227e8c8efac9b02f5465a902910401db397400
Duc Vu <de.yd315@gmail.com>
2019-04-12 09:53:39 -0500

    added routes and linls

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import Login from './components/Login'
+import Home from './components/Home'
+import PrivateRoute from './components/PrivateRoute'
 import './App.css';
 
 
@@ -7,8 +11,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Link to='/login'>Log In</Link>
+        <Link to='/home'>Home</Link>
+
+        <Route path='/login' component={Login}/>
+        <PrivateRoute exact path="/home" component={Home} />
       </div>
+
     );
   }
 }
--- friends/src/index.js
+++ friends/src/index.js
@@ -14,9 +14,12 @@ const store = createStore(friendReducer,
     applyMiddleware(logger));
 
 ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
